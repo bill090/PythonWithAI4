6623c0a906281147397574416aaa194ef7e4e330
William LIAO <william.bw.liao@gmail.com>
2021-08-23 10:22:56 -0400

    Tensorflow stuff.

---
+++ Code/convolution/more_filters.py
@@ -0,0 +1,22 @@
+import math
+import sys
+
+from PIL import Image, ImageFilter
+#Import all the enhancement filter from pillow
+from PIL.ImageFilter import (
+   BLUR, CONTOUR, DETAIL, EDGE_ENHANCE, EDGE_ENHANCE_MORE,
+   EMBOSS, FIND_EDGES, SMOOTH, SMOOTH_MORE, SHARPEN
+)
+
+# Ensure correct usage
+if len(sys.argv) != 2:
+    sys.exit("Usage: python filter.py filename")
+
+# Open image
+image = Image.open(sys.argv[1]).convert("RGB")
+
+# Filter image according to edge detection kernel
+filtered = image.filter(EDGE_ENHANCE_MORE)
+
+# Show resulting image
+filtered.show()
